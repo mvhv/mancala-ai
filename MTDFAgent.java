8c585fce2eb47ee4ebde03c98d6e7e42452183a3
Jesse Wyatt <wyattjrw@gmail.com>
2018-10-20 02:53:00 +0800

    Clarified comments.
    Explained negative ChildMove, use of Date class and cleaned up move() doc string.

--- MTDFAgent.java
+++ MTDFAgent.java
@@ -86,6 +86,7 @@ public class MTDFAgent implements MancalaAgent {
 
   /**
    * Checks to see if the move timer is nearly up.
+   * Uses java.util.Date to avoid overzealous filtering of "Syst*m" calls.
    * 
    * @return true if time is up, otherwise false
    */
@@ -402,24 +403,26 @@ public class MTDFAgent implements MancalaAgent {
   }
 
   /**
-   * Allows the agent to nominate the house the agent would like to move seeds from. 
-   * The agent will allways have control of houses 0-5 with store at 6. 
-   * Any move other than 0-5 will result in a forfeit. 
-   * An move from an empty house will result in a forfeit.
-   * A legal move will always be available.
-   * Assume your agent has 0.5 seconds to make a move. 
-   * @param board the current state of the game. 
-   * The board is an int array of length 14, indicating the 12 houses and 2 stores. 
-   * The agent's house are 0-5 and their store is 6. The opponents houses are 7-12 and their store is 13. Board[i] is the number of seeds in house (store) i.
-   * board[(i+1}%14] is the next house (store) anticlockwise from board[i].  
-   * This will be consistent between moves of a normal game so the agent can maintain a strategy space.
-   * @return the house the agent would like to move the seeds from this turn.
+   * Public API call for requesting moves from the agent.
+   * The game is assumed to be the Kalah(6,3) variant with 6 houses per side, and initially 3 seeds per house.
+   *
+   * The board is an int array of length 14.
+   * Each board entry indicates the number of seeds located in that pit.
+   * The agent's houses are 0-5 and their store is 6. 
+   * The opponent's houses are 7-12 and their store is 13.
+   * Seeds are played anti-clockwise and board ordering is circular with the pit at index 0 following the pit at index 13.
+   *
+   * @param board the current game state
+   * @return the house the agent would like to play from this turn
    */
   public int move(int[] board) {
     int depth = 1;
     int guess = 0;
-    ChildMove state = new ChildMove(-10, board); // this is leaking
     MoveScore best;
+    ChildMove state;
+    
+    // The search must be initialised with an invalid "parent" move to avoid premature trans table association
+    state = new ChildMove(-10, board);
 
     this.searchCutoff = new Date().getTime() + MAX_RUN_TIME;
     best = MTDF(state, guess, depth);
